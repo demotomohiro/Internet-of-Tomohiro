4bacb4915808a51646f62ea926938545a10c3456
demotomohiro <gpuppur@gmail.com>
2020-10-27 16:32:08 +0900

    Update Nim version to 1.4.0

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -u
 src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$src_dir/scripts/installnim.sh"
 
-install_nim_check "1.0.6"
+install_nim_check "1.4.0"
 cd "$src_dir/src"
 nim c -r -d:release "$src_dir/src/bloggen.nim"
