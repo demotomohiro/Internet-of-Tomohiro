a0e8f075453b4e5863116ab60b65dc7ae80e9655
demotomohiro <gpuppur@gmail.com>
2022-05-08 02:51:35 +0900

    Update to Nim 1.6.6

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -u
 src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$src_dir/scripts/installnim.sh"
 
-install_nim_check "1.6.2"
+install_nim_check "1.6.6"
 cd "$src_dir/src"
 nim c -r -d:release "$src_dir/src/bloggen.nim"
